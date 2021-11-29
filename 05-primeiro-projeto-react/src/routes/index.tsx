9b48cecabd409c4ac254659b2c295d77974b26a5
MysaelR <mysael.ti@gmail.com>
2021-11-29 09:07:50 -0300

    criando rotas

---
+++ 05-primeiro-projeto-react/src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/repository';
+
+
+const Routess = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/repository" element={<Repository />} />
+        </Routes>
+    </BrowserRouter>
+
+
+);
+
+export default Routess;
